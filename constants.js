07741c8216f4f806d9e87e775a558f9d23c4e464
Revolution792 <42079744+Revolution792@users.noreply.github.com>
2026-07-08 02:33:51 +0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -155,7 +155,7 @@ export const LANGUAGES_URL = [
     },
     {
         language: "ukrainian",
-        folder: "ua",
+        folder: "uk",
         url: getLanguageUrl("ukrainian"),
     },
     {
